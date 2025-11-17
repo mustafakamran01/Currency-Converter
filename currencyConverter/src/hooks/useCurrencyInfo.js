f92dc1b3ecd077a1c9450f0b508943d94da973db
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-17 12:56:15 +0530

    Changing method in custom hook

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -19,15 +19,16 @@ const useCurrencyInfo = (currency) => {
 
     const [data, setData] = useState({})
 
-    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-    .then( (res) => res.json())
-    .then( (res) => setData(res[currency]))
+    useEffect( () => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then( (res) => res.json())
+        .then( (res) => setData(res[currency]))
+    
+    }, [currency])
 
     return data
 }
 
-useEffect( () => {
-    useCurrencyInfo(currency)
-}, [currency])
+
 
 export default useCurrencyInfo
